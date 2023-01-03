f9658e0a5ef6a268a223e3e036ee6d6da4261b8a
Tyrone Johnson Reddy <tyronejohnson.reddy@gmail.com>
2023-01-03 12:01:45 +0000

    set up fast api and uvicorn web server

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
+from fastapi import FastAPI
+import uvicorn
 from src.logic import wiki
+from src.logic import search_wiki
 
-name = wiki()
-print(name)
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    """root method"""
+    return {"message":"wikipedia API"}
+
+@app.get("/search/{value}")
+async def search(value:str):
+    """search wiki"""
+    result = search_wiki(value)
+    return {"results":result}
+
+if __name__ == "__main__":
+    uvicorn.run(app,port=8000,host='0.0.0.0')
