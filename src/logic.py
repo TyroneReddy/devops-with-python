22ac9fa65f99f56db6b2f5ff899f6c1d803bbba4
Tyrone Johnson Reddy <tyronejohnson.reddy@gmail.com>
2023-01-03 18:06:39 +0000

    linting issue fix

--- src/logic.py
+++ src/logic.py
@@ -1,17 +1,20 @@
 import wikipedia
 from textblob import TextBlob
 
+
 def wiki(name="war god", length=1):
     """this is a wikipedia fetcher"""
 
     my_wiki = wikipedia.summary(name, length)
     return my_wiki
 
+
 def search_wiki(name):
     """search wikipedia by name"""
 
     results = wikipedia.search(name)
     return results
+
 
 def phrase(name):
     """Returns phrases from wikipedia"""
